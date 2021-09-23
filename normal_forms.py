8f07bc7b878c0d67121473d93714a81febd3d5d5
MaxWolf-01 <maximilianwolf.0412@gmail.com>
2021-09-23 22:43:04 +0200

    added normal_forms

---
+++ normal_forms.py
@@ -0,0 +1,53 @@
+from truth_table import TruthTable
+
+
+class NormalForm:
+    def __init__(self, truth_table: TruthTable, results, nf_params):
+        self.TT = truth_table
+        self.TT_results = self.set_results(results)
+        self.NF = self.NF(*nf_params)
+
+    @staticmethod
+    def set_results(results):
+        if type(results) == list:
+            return results
+        return [int(val) for val in results]
+
+    def NF(self, cdnf_else_ccnf: bool, inner_junction: str, outer_junction: str) -> list:
+        if len(self.TT_results) != len(self.TT.table):
+            raise Exception('Invalid Input (result and truth table rows do not match in length)')
+        nf = []
+        for i in range(len(self.TT.table)):
+            if self.TT_results[i] == cdnf_else_ccnf:
+                if nf:
+                    nf.append(outer_junction)
+                nf.append('(')
+                row = self.TT.table[i]
+                for j in range(len(self.TT.variables)):
+                    if j != 0:
+                        nf.append(inner_junction)
+                    var = self.TT.variables[j]
+                    if row[j] if cdnf_else_ccnf else not row[j]:
+                        nf.append(var)
+                    else:
+                        nf.append('¬')
+                        nf.append(var)
+                nf.append(')')
+        return nf
+
+    def print(self):
+        print(" ".join(self.NF))
+
+
+class CDNF(NormalForm):
+    def __init__(self, truth_table, results):
+        super(CDNF, self).__init__(truth_table, results, [True, '∧', '∨'])
+
+
+class CCNF(NormalForm):
+    def __init__(self, truth_table, results):
+        super(CCNF, self).__init__(truth_table, results, [False, '∨', '∧'])
+
+
+# x = CCNF(TruthTable(['p', 'q', 'r'], True), '10100100')
+# x.print()
