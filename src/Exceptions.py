5a6663e92bb4076b9b977d5fa465f061a375a12b
MaxWolf-01 <maximilianwolf.0412@gmail.com>
2021-11-30 19:54:27 +0100

    added InvalidOperatorException

--- src/Exceptions.py
+++ src/Exceptions.py
@@ -1,3 +1,6 @@
+from operator_symbols import OPERATORS
+
+
 class BracketException(Exception):
     def __init__(self, expr, msg):
         super(Exception, self).__init__(f'Number of opening and closing brackets do not match: {msg[0]} != {msg[1]} '
@@ -8,3 +11,10 @@ class InvalidExpressionException(Exception):
     def __init__(self, msg):
         super(InvalidExpressionException, self).__init__(f'Invalid expression: {msg}')
 
+
+class InvalidOperatorException(InvalidExpressionException):
+    def __init__(self, operator=''):
+        nl = '\n'
+        info = f"Invalid operator in expression '{operator}'.\nSupported operators are:\n" \
+               f"{nl.join(str(OPERATORS[k]) for k in OPERATORS)}"
+        super(InvalidOperatorException, self).__init__(info)
